324d334b958e0e010f9b30f00a5dc44c75c524d2
rafael-gonzalez-7r1ck <rgonzalezfro@outlook.com>
2024-06-30 16:50:13 -0300

    Pause screen

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject exitButton;
+
+    private void Start()
+    {
+        if (Application.isEditor
+            || Application.platform == RuntimePlatform.WebGLPlayer
+            || Application.isMobilePlatform)
+        {
+            exitButton.SetActive(false);
+        }
+    }
+    public void LoadLevel()
+    {
+        SceneLoader.Instance.LoadScene("GUITest");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
