d01e6b579ca198da246f2931f67741f4a24e65c4
PabloVallejos <pablovallejosmil@hotmail.com>
2024-07-01 15:54:15 -0300

    Empezando el primer nivel
    Añadido animación del conejo.
    Aumentado el limite del poder de salto.
    Armado la primera parte del nivel.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,18 +9,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float minPower = 1;
     [SerializeField]
-    private float maxPower = 10;
+    private float maxPower = 100;
     [SerializeField]
     Vector2 jumpDirection = new Vector2(1, 2);
 
     //public Rigidbody2D head;
     Rigidbody2D rb;
+    Animator anima;
     float power;
     bool ground;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anima = GetComponent<Animator>();
     }
 
     private void Update()
@@ -37,12 +39,16 @@ public class PlayerController : MonoBehaviour
             {
                 power += Time.deltaTime * jumpMultiplier;
             }
+            //power += Time.deltaTime * jumpMultiplier;
+            Debug.Log(power);
         }
 
         if (Input.GetKeyUp(KeyCode.Space) && ground)
         {
             rb.AddForce(jumpDirection.normalized * power);
             power = minPower;
+            anima.SetBool("Jumping", true);
+            anima.SetBool("Landing", false);
         }
 
         //if (head != null)
@@ -53,7 +59,12 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Platform")) { ground = true; }
+        if (collision.gameObject.CompareTag("Platform")) 
+        { 
+            ground = true;
+            anima.SetBool("Jumping", false);
+            anima.SetBool("Landing", true);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
